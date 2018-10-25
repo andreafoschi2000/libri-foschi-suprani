d05acf7e28007c51334842e65722f55f7826b39a
alessandrosuprani <alessandro.suprani.ittbpascal@gmail.com>
2018-10-25 09:37:42 +0200

    Versione finale, non collaudata

--- libreria/MainWindow.xaml.cs
+++ libreria/MainWindow.xaml.cs
@@ -29,7 +29,8 @@ namespace libreria
         {
             InitializeComponent();
         }
-        
+
+        // RICERCA LIBRO PER AUTORE | QUERY 1
         private void btn_libridi_Click(object sender, RoutedEventArgs e)
         {
             lst_lista.Items.Clear();
@@ -51,6 +52,7 @@ namespace libreria
 
         }
 
+        // COPIE PER TITOLO | QUERY 2
         private void btn_copie_Click(object sender, RoutedEventArgs e)
         {
             lst_lista.Items.Clear();
@@ -65,13 +67,15 @@ namespace libreria
 
         }
 
+
+        // NUMERO DI COPIE PER GENERE | QUERY 3
         private void btn_genere_Click(object sender, RoutedEventArgs e)
         {
             lst_lista.Items.Clear();
 
             IEnumerable<string> titles = from libri in XDocument.Load(@"../../libri.XML")
                                         .Elements("Biblioteca").Elements("wiride")
-                                         where (string)libri.Element("genere") == "romanzo"
+                                         where libri.Element("genere").Value.Contains("romanzo")
                                          select libri.Element("titolo").Value;
 
             int cont = 0;
@@ -82,12 +86,39 @@ namespace libreria
             lst_lista.Items.Add(cont.ToString());
         }
 
+
+        // RIMOZIONE TAG ABSTRACT | QUERY 4
         private void btn_remove_Click(object sender, RoutedEventArgs e)
         {
-            XElement elment = (from xml1 in doc.Descendants("abstract")
-                               select xml1).FirstOrDefault();
-            elment.Remove();
-            doc.Save(@"../../libri.XML");
+
+            
+         
+        }
+
+        private void btn_modifica_Click(object sender, RoutedEventArgs e)
+        {
+            string titolo = txt_titolo2.Text;
+            string testo = txt_inputtesto.Text;
+            IEnumerable<XElement> Mod_Gen = from biblioteca in doc.Descendants("wiride")
+
+                                            where biblioteca.Element("titolo").Value == titolo
+
+                                            select biblioteca.Element("genere");
+            if (Mod_Gen.OfType<XElement>().First().Value == null)
+            {
+                doc.Element("Biblioteca")
+               .Elements("wiride")
+               .Where(x => x.Attribute("titolo").Value == titolo).First()
+               .AddBeforeSelf(
+               new XElement("genere", testo));
+            }
+            else
+            {
+                Mod_Gen.OfType<XElement>().First().Value = testo;
+            }
+
+            doc.Save(@"../../libri.xml");
+
         }
     }
 }
